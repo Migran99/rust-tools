c8f890eeb29694074c227250bb8fec728c283920
Mike <miguelgranero99@gmail.com>
2022-12-29 15:47:01 +0100

    all in mod

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,51 @@
 use std::env;
-use std::fs;
 
-struct  CsvFile {
-    name: String,
-    data: Vec<u8>
-}
-
-impl CsvFile {
-    fn load_csv(filename: &String) -> Result<Vec<u8>, &str>{
-        /*
-            Load a csv file
-        */
-        let file_fs = match fs::read(filename) {
-            Ok(f) => {f},
-            Err(_) => return Err("Cannot load file"),
-        };
-    
-        Ok(file_fs)
-    
-    }
-
-    fn new(filename: &String) -> Result<CsvFile, &str>{
-        let data = match CsvFile::load_csv(&filename) {
-            Ok(data) => {data},
-            Err(e) => {return Err(e);},
-        };
-
-        let file = CsvFile{name: filename.clone(), data: data};
-
-        Ok(file)
-
-    }
-
-    fn len(&self) -> usize{
-        self.data.len()
-    }
-    fn name(&self) -> String{
-        self.name.clone()
+mod csv{
+    use std::fs;
+    pub struct  CsvFile {
+        name: String,
+        data: Vec<u8>
     }
     
-}
+    impl CsvFile {
+        fn load_csv(filename: &String) -> Result<Vec<u8>, &str>{
+            /*
+                Load a csv file
+            */
+            let file_fs = match fs::read(filename) {
+                Ok(f) => {f},
+                Err(_) => return Err("Cannot load file"),
+            };
+        
+            Ok(file_fs)
+        
+        }
+    
+        pub fn new(filename: &String) -> Result<CsvFile, &str>{
+            let data = match CsvFile::load_csv(&filename) {
+                Ok(data) => {data},
+                Err(e) => {return Err(e);},
+            };
 
+            let file = CsvFile{name: filename.clone(), data: data};
+
+            Ok(file)
+
+        }
+    
+        pub fn len(&self) -> usize{
+            self.data.len()
+        }
+        pub fn name(&self) -> String{
+            self.name.clone()
+        }
+        
+    }
+
+}    
 
 fn main() {
+    use csv::CsvFile;
     let arguments = env::args();
     let mut filename: String = "test.csv".to_string();
     for (index,argument) in arguments.enumerate(){
