df62fecbf5ad396654edf0ee48ab2980763535e9
Mike <miguelgranero99@gmail.com>
2023-01-08 20:45:26 +0100

    added content print and add line

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,20 @@ use std::env;
 
 mod csv{
     use std::fs;
+    use std::str;
     pub struct  CsvFile {
         name: String,
-        data: Vec<u8>
+        data: Vec<u8>,
+        metadata: Metadata
+    }
+
+    struct Metadata {
+        item_number: u32,
     }
     
     impl CsvFile {
+
+        // UAX FUNCTIONS
         fn load_csv(filename: &String) -> Result<Vec<u8>, &str>{
             /*
                 Load a csv file
@@ -20,6 +28,39 @@ mod csv{
             Ok(file_fs)
         
         }
+
+        fn content_string(&self) -> &str {
+            let s = match str::from_utf8(&self.data) {
+                Ok(v) => v,
+                Err(e) => panic!("Invalid UTF-8 content: {}", e),
+            };
+
+            s
+        }
+
+        fn apped_bytes(&mut self, data: Vec<u8>){
+            let mut copy = data.clone();
+            self.data.append(&mut copy);
+        }
+        fn apped_data(&mut self, data: &str) {
+            self.apped_bytes(data.as_bytes().to_vec());
+
+        }
+
+        fn get_item_number(&self) -> u32 {
+            let content = self.content_string();
+            let header = content.lines().next().unwrap();
+
+            let header_items: Vec<&str> = header.split(',').collect(); 
+            
+            u32::try_from(header_items.len()).unwrap()
+        }
+
+        pub fn init(&mut self){
+            self.metadata.item_number = self.get_item_number();
+        }
+
+        // +++++++++++++++++++++++
     
         pub fn new(filename: &String) -> Result<CsvFile, &str>{
             let data = match CsvFile::load_csv(&filename) {
@@ -27,7 +68,9 @@ mod csv{
                 Err(e) => {return Err(e);},
             };
 
-            let file = CsvFile{name: filename.clone(), data: data};
+            let mut file = CsvFile{name: filename.clone(), data: data, metadata: Metadata { item_number: 0 }};
+            file.init();
+
 
             Ok(file)
 
@@ -39,10 +82,42 @@ mod csv{
         pub fn name(&self) -> String{
             self.name.clone()
         }
+
+        pub fn print_content(&self) {  
+            let content = self.content_string();
+            println!("####################");
+
+            for (i,l) in content.lines().enumerate(){
+                if i == 0
+                    {println!("{}",l.to_uppercase());}
+                else
+                    {println!("{}",l);}
+            }
+            println!("####################");
+        }
+
+        pub fn add_line(&mut self, data: &mut [&str]) {
+            self.apped_data("\n");
+
+            for i in 0..self.metadata.item_number {
+                if i < self.metadata.item_number-1 {
+                    if i < data.len().try_into().unwrap() {
+                        let a = data[usize::try_from(i).unwrap()];
+                        self.apped_data(a);
+                    }
+                    self.apped_data(",");
+                }
+            }
+
+
+
+        }
+
+
         
     }
 
-}    
+}   
 
 fn main() {
     use csv::CsvFile;
@@ -55,10 +130,14 @@ fn main() {
         }
     }
 
-    let my_file = match CsvFile::new(&filename) {
-        Ok(my_file) => {my_file},
-        Err(_) => {return;},
-    };
+    let mut my_file = CsvFile::new(&filename).unwrap();
 
-    print!("File {} size in Bytes: {}", my_file.name(), my_file.len());
+    println!("File {} size in Bytes: {}", my_file.name(), my_file.len());
+
+    my_file.print_content();
+    my_file.add_line(&mut ["a"]);
+
+    my_file.print_content();
+
+
 }
